f2c6b23fe4d2ee08130c6a006718af2aade9c7a5
Ksenni888 <130316443+Ksenni888@users.noreply.github.com>
2025-07-15 08:54:34 +0300

    project done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -52,7 +52,6 @@ public class Main {
     }
 
     private void breakdown(List<String>filesNames, boolean addTofile) {
-        log.info("Breakdown");
         int counter_integers = 0;
         int counter_floats = 0;
         int counter_strings = 0;
